f1b5d78edf0320d4bc846be27eac606746b6f659
Jignesh Desai <jignesh_desai@freytag.us>
2024-05-09 17:52:40 +0530

    Add project files.

---
+++ ClassLibrarydotnetStandard2/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.Services.Store;
+
+namespace ClassLibrarydotnetStandard2
+{
+    public class Class1
+    {
+        public string LibraryFunctionToGetPackageName()
+        {
+
+            // will only work if you include Windows SDK Contract nuget package
+
+            Windows.ApplicationModel.Package package = Windows.ApplicationModel.Package.Current;
+            var Packageversion = new Version(package.Id.Version.Major, package.Id.Version.Minor, package.Id.Version.Build);
+            var PackageDisplayName = package.DisplayName;
+            var PackageUniqueName = package.Id.Name;
+            return (string.Format("{0} - {1}", PackageDisplayName, Packageversion.ToString()));
+        }
+
+        public bool StoreInit()
+        {
+            StoreContext s = StoreContext.GetDefault();
+            if (s==null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ WpfAppdotnet8/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppdotnet8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            ClassLibrarydotnetStandard2.Class1 c = new ClassLibrarydotnetStandard2.Class1();
+            this.Title = c.LibraryFunctionToGetPackageName();
+        }
+
+
+     
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            // trying calling WPFFunctionToGetPackageName()
+            // After you have changed OS target to windows 10 from Project properties
+            this.Title = WPFFunctionToGetPackageName();
+        }
+
+        public string WPFFunctionToGetPackageName()
+        {
+            // Will not work until you set OS target Window 10
+
+            //Windows.ApplicationModel.Package package = Windows.ApplicationModel.Package.Current;
+            //var Packageversion = new Version(package.Id.Version.Major, package.Id.Version.Minor, package.Id.Version.Build);
+            //var PackageDisplayName = package.DisplayName;
+            //var PackageUniqueName = package.Id.Name;
+            //return (string.Format("{0} - {1}", PackageDisplayName, Packageversion.ToString()));
+            return "";
+        }
+    }
+}
